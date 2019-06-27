1f7108af35c831b294c9d68425a6bb1878c171c2
ufologist <klsforever@gmail.com>
2019-06-27 16:47:53 +0800

    添加构建的配置

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import {
+    uglify
+} from 'rollup-plugin-uglify';
+
+var plugins = [
+    nodeResolve(),
+    commonjs(),
+    babel({
+        exclude: 'node_modules/**'
+    }),
+    uglify()
+];
+
+export default [{
+    input: 'src/standard-http-client.js',
+    output: {
+        file: 'dist/standard-http-client.js',
+        format: 'umd',
+        name: 'StandardHttpClient'
+    },
+    plugins: plugins
+}];
